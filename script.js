7f9317f19078eea937b1513b5f647b8c94454a36
Abby Fernandes <fernande.a@maristcollege.school.nz>
2021-10-10 15:43:36 +1300

    search animation removed

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ function showMenu() {
         link.classList.toggle("fade");
     });
 };
+
+function openSearch() {
+    let searchBar = document.querySelector('#search-bar')
+    textArea = document.querySelector('#search-text')
+    cancelButton = document.querySelector('#search-cancel')
+    nav = document.querySelector("nav");
+
+    searchBar.classList.add("active");
+    textArea.classList.add("active");
+    cancelButton.classList.add("active");
+
+    nav.style.display = "none";
+};
+
+function closeSearch() {
+    let searchBar = document.querySelector('#search-bar')
+    textArea = document.querySelector('#search-text')
+    cancelButton = document.querySelector('#search-cancel');
+    nav = document.querySelector("nav");
+
+    searchBar.classList.remove("active");
+    textArea.classList.remove("active");
+    cancelButton.classList.remove("active");
+
+    nav.style.display = "block";
+
+    textArea.value = "";
+
+};
+
